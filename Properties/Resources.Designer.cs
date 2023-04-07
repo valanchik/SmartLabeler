f94d9bf4362c4a138ddc1a601f9d1fa85ea52592
admin <admin@vovanchik.ru>
2023-04-07 13:14:03 +0300

    Rename Prooject

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ProcScan.Properties {
+namespace SmartLabeler.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace ProcScan.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ProcScan.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SmartLabeler.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- RectSelector/DrawingRectangle.cs
+++ RectSelector/DrawingRectangle.cs
@@ -2,7 +2,7 @@
 using System;
 using System.Drawing;
 
-namespace ProcScan.RectSelector
+namespace SmartLabeler.RectSelector
 {
     public class DrawingRectangle : IScalible
     {
